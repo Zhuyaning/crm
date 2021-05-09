ccc4608a4bc016ce18d085328c3fd4b58746b6b1
WanvanHhh <2646492006@qq.com>
2021-05-09 14:55:58 +0800

    second commit

---
+++ src/main/java/com/notfound/crm/sys/interceptor/MyInterceptor.java
@@ -0,0 +1,33 @@
+package com.notfound.crm.sys.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Wan_JiaLin
+ * @Create 2021-05-08 19:24
+ * @Description: 这里是员工类的拦截器
+ */
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getSession().getAttribute("CURR_USER") == null){ //验证是否登录，如果为空，则用户未登录
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
--- src/main/java/com/notfound/crm/sys/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/notfound/crm/sys/service/impl/EmployeeServiceImpl.java
@@ -10,6 +10,8 @@ import com.notfound.crm.sys.vo.EmployeeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Wan_JiaLin
  * @Create 2021-04-30 10:12
@@ -31,13 +33,18 @@ public class EmployeeServiceImpl extends BaseServiceImpl<EmployeeForm> implement
      * @param employeeForm
      * @return
      */
-    public Result login(EmployeeForm employeeForm){
+    public Result login(EmployeeForm employeeForm, HttpServletRequest request){
         EmployeeVO data = employeeMapper.login(employeeForm);
         if(data == null){   //查询记过如果为空，则该用户名错误
             return new Result(CodeMsg.LOGIN_FAILD_WRONG_USERNAME);    //登录失败，用户名错误
         }else { //查询结果不为空，证明改用户名正确
             //判断该用户名对应的密码是否与数据库中的密码相同，如果相同，那么则允许该用户登录系统，若不相同则返回错误信息
             if(data.getPassword().equals(employeeForm.getPassword())){
+                request.getSession().setAttribute("CURR_USER",data);
+                System.out.println("在此取出存入的session内容"+request.getSession().getAttribute("CURR_USER"));
+                EmployeeVO curr_user = (EmployeeVO) request.getSession().getAttribute("CURR_USER");
+
+                System.out.println(curr_user.getName());
                 return new Result();    //登录成功
             }else {
                 return new Result(CodeMsg.LOGIN_FAILD_WRONG_PASSWORD); //登录失败，密码错误
