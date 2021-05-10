af1bb973caf637ccdadf7e521213fd3014eb9ba8
Zhuyaning <2495454242@qq.com>
2021-05-10 20:29:06 +0800

    客户Controller修改

--- src/main/java/com/notfound/crm/sys/controller/PotentialcustomerController.java
+++ src/main/java/com/notfound/crm/sys/controller/PotentialcustomerController.java
@@ -2,10 +2,15 @@ package com.notfound.crm.sys.controller;
 
 import com.notfound.crm.common.base.Query;
 import com.notfound.crm.common.base.Result;
+import com.notfound.crm.common.validator.ValidatorUtil;
+import com.notfound.crm.sys.domain.Potentialcustomer;
+import com.notfound.crm.sys.form.PotentialcustomerForm;
 import com.notfound.crm.sys.service.IPotentialcustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /***
  *   Created by IntelliJ IDEA.
@@ -13,34 +18,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *   Date: 2021/5/10
  *   Time: 11:19
  */
-@Controller
-@RequestMapping("po")
+@RestController
+@RequestMapping("customer")
 public class PotentialcustomerController {
 
-  @Autowired
-  IPotentialcustomerService iPotentialcustomerService;
+    @Autowired
+    IPotentialcustomerService iPotentialcustomerService;
 
-  /***
-   * 根据关键字查询客户信息，实现分页。
-   * @param query
-   * @return
-   */
-  @RequestMapping("/queryPage")
-  public Result queryPage(Query query){
+    /***
+     * 根据关键字查询客户信息，实现分页。
+     * @param query
+     * @return
+     */
+    @RequestMapping("/queryPage")
+    public Result queryPage(Query query){
 
-    Result result = iPotentialcustomerService.queryPage(query);
-    return result;
-  }
+        Result result = iPotentialcustomerService.queryPage(query);
+        return result;
+    }
 
-  /***
-   * 根据id删除
-   * @param id
-   * @return
-   */
-  @RequestMapping("/delete")
-  public Result deleteById(Integer id){
-    Result result = iPotentialcustomerService.delete(id);
-    return result;
-  }
+    /***
+     * 增加一个客户信息
+     * @param form
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(PotentialcustomerForm form){
+
+        ValidatorUtil.validator(form);//验证前端传入数据非空
+        Result result = iPotentialcustomerService.add(form);
+        return result;
+    }
+
+    /***
+     * 根据id删除
+     * @param id
+     * @return
+     */
+    @RequestMapping("/delete")
+    public Result deleteById(Integer id){
+        Result result = iPotentialcustomerService.delete(id);
+        return result;
+    }
+
+    /***
+     * 修改客户对象
+     * @param form
+     * @return
+     */
+    @RequestMapping("/update")
+    public Result update(PotentialcustomerForm form){
+        ValidatorUtil.validator(form);
+        Result result = iPotentialcustomerService.update(form);
+        return result;
+    }
 
 }
